f6bf7180b6c983944edc503b7bf4b6fb677218e3
KKerb9 <lukinkd.prog@gmail.com>
2026-05-22 12:33:13 +0300

    add base structure

---
+++ src/ml/core/__init__.py
@@ -0,0 +1,27 @@
+from .types import (
+    AnnotationSet,
+    AnnotationSetLine,
+    Case,
+    EvaluationReport,
+    MetricResult,
+    MetricValue,
+    ParticipantKind,
+    Role,
+    Span,
+    TextDocument,
+    TextLine,
+)
+
+__all__ = [
+    "AnnotationSet",
+    "AnnotationSetLine",
+    "Case",
+    "EvaluationReport",
+    "MetricResult",
+    "MetricValue",
+    "ParticipantKind",
+    "Role",
+    "Span",
+    "TextDocument",
+    "TextLine",
+]
